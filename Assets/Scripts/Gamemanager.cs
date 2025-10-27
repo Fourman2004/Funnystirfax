9eaa4ae546bae75a6757be9f475ec94969de3a38
Marshall Sharp <76953814+Fourman2004@users.noreply.github.com>
2025-10-27 17:56:47 +0000

    Game Manager finished?

--- Assets/Scripts/Gamemanager.cs
+++ Assets/Scripts/Gamemanager.cs
@@ -1,49 +1,76 @@
+using System;
 using System.Collections;
+using System.IO;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 
-
+[Serializable]
+public class PlayerData
+{
+    public int highscore;
+    public string path;
+    public int score;
+}
 
 public class Jsonfilemanager
 {
-    public int highscore, score;
 
-    public void filefinder()
+
+    public bool filefinder(PlayerData data)
     {
-        string path = Application.persistentDataPath + "/savefile.json";
-        if (System.IO.File.Exists(path))
+        if (System.IO.File.Exists(data.path))
         {
-            string json = System.IO.File.ReadAllText(path);
-            Jsonfilemanager data = JsonUtility.FromJson<Jsonfilemanager>(json);
-            SaveScore(data);
+            Debug.Log("File found at" + data.path);
+            return true;
         }
         else
         {
-            Debug.LogWarning("No save file found");
-            System.IO.File.Create(path);
-            filefinder();
+            Debug.LogWarning("File not found at" + data.path);
+            return false;
+        }
+
+    }
+
+    public void LoadScore(PlayerData data)
+    {
+        if (filefinder(data))
+        {
+            string filecontents = System.IO.File.ReadAllText(data.path);
+            var loadedData = JsonUtility.FromJson<PlayerData>(filecontents);
+            if (loadedData != null)
+            {
+                data.highscore = loadedData.highscore;
+            }
+            
         }
     }
 
-    public void SaveScore(Jsonfilemanager data)
+    public void SaveScore(PlayerData data)
     {
-        if (score > highscore)
+        if (data.score > data.highscore && filefinder(data))
         {
-         data.highscore = score;
+            data.highscore = data.score;
+            Debug.Log("New Highscore: " + data.highscore);
+            string filecontents = JsonUtility.ToJson(data, true);
+            System.IO.File.WriteAllText(data.path, filecontents);
+            Debug.Log("Saved");
         }
-
     }
 }
 
 
-    public class Gamemanager : MonoBehaviour
+public class Gamemanager : MonoBehaviour
 {
 
     public GameState m_currentState;
-    public int score, highScore;
+    public int playerScore;
+    public GameObject[] Menus;
     public Jsonfilemanager jsonfilemanager;
-    bool doOnce = false;
+    public float timer = 0.2f;
+    public PlayerData playerData;
+    public bool[] doOnce;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public enum GameState
@@ -57,8 +84,19 @@ public class Jsonfilemanager
     void Start()
     {
         jsonfilemanager = new Jsonfilemanager();
-    }
+        playerData.path = Application.persistentDataPath + "/savefile.txt";
+        if (!System.IO.File.Exists(playerData.path))
+        {
+            playerData = new PlayerData();
+            Debug.LogWarning("No save file found");
+            System.IO.File.Create(playerData.path);
+        }
+        else
+        {
+            jsonfilemanager.LoadScore(playerData);
+        }
 
+    }
     void Update()
     {
         switch (m_currentState)
@@ -66,24 +104,70 @@ public class Jsonfilemanager
 
             case GameState.MainMenu:
                 // Handle main menu logic
+                if (!doOnce[0])
+                {
+                    Instantiate(Menus[0]);
+                    doOnce[0] = true;
+                }
+                Debug.Log("Main Menu");
                 break;
             case GameState.Playing:
-                // Handle gameplay logic
+                // Handle playing logic
+                if (!doOnce[1])
+                {
+                    Time.timeScale = 1f;
+                    doOnce[1] = true;
+                    StartCoroutine(Scoring());
+                    clearMenus();
+                }
+                Debug.Log("Playing");
                 break;
             case GameState.Paused:
                 // Handle paused logic
+                if (doOnce[2])
+                {
+                    StopAllCoroutines();
+                    Time.timeScale = 0f;
+                    doOnce[2] = false;
+                }
+                Debug.Log("Paused");
+
                 break;
             case GameState.GameOver:
                 // Handle game over logic
-                if (!doOnce)
+                if (!doOnce[3])
                 {
-                    jsonfilemanager.filefinder();
-                    doOnce = true;
+                    StopAllCoroutines();
+                    playerData.score = playerScore;
+                    Instantiate(Menus[2]);
+                    jsonfilemanager.SaveScore(playerData);
+                    doOnce[3] = true;
                 }
+                Debug.Log("Game Over");
                 break;
 
         }
     }
 
-        // Update is called once per frame
+    IEnumerator Scoring()
+    {
+        while (m_currentState == GameState.Playing)
+        {
+            yield return new WaitForSeconds(timer);
+            playerScore++;
+            Debug.Log("Score: " + playerScore);
+        }
     }
+    // Update is called once per frame
+
+
+    void clearMenus()
+    {
+        for (int i = 0; i < Menus.Length; i++)
+        {
+            Destroy(GameObject.FindWithTag("Menu"));
+
+        }
+    }
+}
+
