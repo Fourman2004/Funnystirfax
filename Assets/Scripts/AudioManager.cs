d3e02a876e160fa7fe8e03ad3eda4f8df2bd742b
Marshall Sharp <76953814+Fourman2004@users.noreply.github.com>
2025-12-13 23:56:47 +0000

    Finish up today

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public AudioSource backgroundMusicSource;
+    public AudioClip[] SFX;
+    Gamemanager gameManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<Gamemanager>();
+        backgroundMusicSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameManager.m_currentState == Gamemanager.GameState.GameOver)
+        {
+            backgroundMusicSource.Stop();
+        }
+    }
+
+    public void PlaySFX(int index)
+    {
+        AudioSource.PlayClipAtPoint(SFX[index], Vector3.zero/*transform here*/);
     }
 }
